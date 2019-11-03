e1967d432b41cb0f1170988bac764e50a75482c3
Matsuyuki/rmn. (@rmn_e) <matsuyuki.eca@gmail.com>
2019-11-03 18:33:30 +0900

    ci(travis-release): 新バージョン配布と同時にサンプルも最新化する

--- scripts/travis-release.sh
+++ scripts/travis-release.sh
@@ -7,6 +7,7 @@ if [ $? -eq 1 ]; then
     npm start
     WWA_WING_VERSION=$(npm run print-version --silent)
     echo "{\"version\":\"$WWA_WING_VERSION\"}" > ./wwawing.com/latest-version.json 
+    cp -R ./output/wwawing-update/*.* ./wwawing.com/wing
     git add -A
     git commit -m "[skip travis] RELEASED WWA Wing v$WWA_WING_VERSION"
     if [ $? -eq 0 ]; then
