9e2708f1ac34279a5f2c7f9bc1b812a5db21e039
Aokashi <Aokashi.Towa@Gmail.com>
2020-08-15 22:56:16 +0900

    index.js を TypeScript 化

--- blog.wwawing.com/src/pages/index.tsx
+++ blog.wwawing.com/src/pages/index.tsx
@@ -1,12 +1,35 @@
 import React from "react"
-import { Link, graphql } from "gatsby"
+import { PageProps, Link, graphql } from "gatsby"
 
 import Bio from "../components/bio"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { rhythm } from "../utils/typography"
 
-const BlogIndex = ({ data, location }) => {
+type DataProps = {
+  site: {
+    siteMetadata: {
+      title: string
+    }
+  },
+  allMarkdownRemark: {
+    edges: {
+      node: {
+        excerpt: string,
+        fields: {
+          slug: string
+        }
+        frontmatter: {
+          date: string,
+          title: string,
+          description: string
+        }
+      }
+    }[]
+  }
+}
+
+const BlogIndex: React.FC<PageProps<DataProps>> = ({ data, location }) => {
   const siteTitle = data.site.siteMetadata.title
   const posts = data.allMarkdownRemark.edges
 
