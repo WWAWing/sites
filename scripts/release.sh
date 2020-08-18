68687847cd8bc08e90a1f219b356b2eeda14c2f8
Matsuyuki <matsuyuki.eca@gmail.com>
2020-08-18 23:51:05 +0900

    fix: travis廃止, netlifyがHTMLを生成するだけにする

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,59 +1,20 @@
 #!/bin/sh
 
 # リリーススクリプト
-# - travis の master ブランチへのコミット時 -> 配布物生成 + 配布物コミット + push
-# - netlify の master ブランチへのコミット時 -> 何もしない
-# - netlify の Pull Request のプレビュー生成時 -> 配布物生成 (-> netlify にデプロイ) 
-
-IS_PREVIEW=0
+# - travis の master ブランチへのコミット時 -> 何もしない
+# - netlify の master ブランチへのコミット時 -> HTML生成＋デプロイ
+# - netlify の Pull Request のプレビュー生成時 -> HTML生成＋デプロイ
 
 if [ -n "$TRAVIS" ];  then
-  RELEASE_BRANCH=$TRAVIS_BRANCH
-  git checkout $RELEASE_BRANCH
+  echo "Travis でのビルドは廃止されました"
+  exit 0
 fi
 
 if [ -n "$NETLIFY" ]; then 
   npm run fill-version
+  npm run generate-download-page
   if [ $? -eq 1 ]; then
-    echo "fill-version runtime error."
+    echo "runtime error."
     exit 1
   fi
-  if [ -z $PULL_REQUEST -o "$PULL_REQUEST" = false ]; then
-    echo "[netlify master branch deploy] skip deploying new version."
-    exit 0
-  fi
-  echo "[netlify PR preview deploy] preview mode"
-  IS_PREVIEW=1
-fi
-
-npm run check-released --silent
-
-if [ $? -eq 0 ]; then
-  echo "[up to date] nothing to release."
-  exit 0
-fi
-
-npm start
-if [ $? -eq 1 ]; then
-  echo "build error."
-  exit 1
-fi
-
-WWA_WING_VERSION=$(npm run print-version --silent)
-cp -R ./node_modules/@wwawing/all/dist/wwawing-update/*.* ./wwawing.com/wing
-
-if [ $? -ne 0 ]; then
-  echo "failed to update wwawing example."
-  exit 1
-fi
-
-if [ $IS_PREVIEW -eq 1 ]; then
-  exit 0
-fi
-
-git add -A
-git commit -m "[skip travis] RELEASED WWA Wing v$WWA_WING_VERSION"
-
-if [ $? -eq 0 ]; then
-  git push origin $RELEASE_BRANCH
 fi
