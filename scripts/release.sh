09be2885d4329e66807f5711ff7d84572d0e7aa5
Matsuyuki <matsuyuki.eca@gmail.com>
2020-08-19 22:29:39 +0900

    refactor: travis連携削除, スクリプトとREADMEの整理

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,18 +1,7 @@
 #!/bin/sh
 
-# リリーススクリプト
-# - travis の master ブランチへのコミット時 -> 何もしない
-# - netlify の master ブランチへのコミット時 -> HTML生成＋デプロイ
-# - netlify の Pull Request のプレビュー生成時 -> HTML生成＋デプロイ
-
-if [ -n "$TRAVIS" ];  then
-  echo "Travis でのビルドは廃止されました"
-  exit 0
-fi
-
 if [ -n "$NETLIFY" ]; then 
-  npm run fill-version
-  npm run generate-download-page
+  npm start
   if [ $? -eq 1 ]; then
     echo "runtime error."
     exit 1
