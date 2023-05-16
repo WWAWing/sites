96f97fadf3b598cd82804c90021731ce7c9f0760
Matsuyuki <matsuyuki.eca@gmail.com>
2023-05-16 23:15:14 +0900

    acorn-statement バージョンデプロイ

--- scripts/deploy-demo.sh
+++ scripts/deploy-demo.sh
@@ -9,22 +9,13 @@ unzip -d wwawing-dist wwawing-dist.zip
   
 SRC_DIR="./wwawing-dist/wwawing-dist"
 DEST_DIR="./wwawing.com/wing"
-## new-message-system バージョンを公開していた時のスクリプトをコメントアウトしています。
-## 今後、不安定版のプレビューが必要になった時はコメントアウトを外していい感じに改変してください。
-## HACK: プレビューをデプロイすべきタグ名を、releases.json の releaseUnits[0] から自動で取得して回したい。
 
-## DEST_DIR_NEW_MESSAGE_SYSTEM="./wwawing.com/new-message-system"
 cp "$SRC_DIR/manual.html" $DEST_DIR
 cp "$SRC_DIR/mapdata/wwamap.dat" $DEST_DIR
-# cp "$SRC_DIR/mapdata/wwamap.dat" $DEST_DIR_NEW_MESSAGE_SYSTEM
 cp "$SRC_DIR/mapdata/wwa.js" $DEST_DIR
-# cp "$SRC_DIR/mapdata/wwa.js" $DEST_DIR_NEW_MESSAGE_SYSTEM
 cp "$SRC_DIR/mapdata/wwa.css" $DEST_DIR
-# cp "$SRC_DIR/mapdata/wwa.css" $DEST_DIR_NEW_MESSAGE_SYSTEM
 cp "$SRC_DIR/mapdata/style.css" $DEST_DIR
-# cp "$SRC_DIR/mapdata/style.css" $DEST_DIR_NEW_MESSAGE_SYSTEM
 cp "$SRC_DIR/mapdata/making.gif" $DEST_DIR
-# cp "$SRC_DIR/mapdata/making.gif" $DEST_DIR_NEW_MESSAGE_SYSTEM
 cp "$SRC_DIR/mapdata/island02.dat" $DEST_DIR
 cp "$SRC_DIR/mapdata/island02.gif" $DEST_DIR
 cp "$SRC_DIR/mapdata/caves01.dat" $DEST_DIR
@@ -32,8 +23,24 @@ cp "$SRC_DIR/mapdata/caves01.gif" $DEST_DIR
 cp "$SRC_DIR/mapdata/caves02.dat" $DEST_DIR
 cp "$SRC_DIR/mapdata/caves02.gif" $DEST_DIR
 cp "$SRC_DIR/mapdata/cover.gif" $DEST_DIR
-# cp "$SRC_DIR/mapdata/cover.gif" $DEST_DIR_NEW_MESSAGE_SYSTEM
 cp -R "$SRC_DIR/mapdata/audio" $DEST_DIR
-# cp -R "$SRC_DIR/mapdata/audio" $DEST_DIR_NEW_MESSAGE_SYSTEM
 
-rm -rf ./wwawing-dist ./wwawing-dist.zip
+rm -rf $SRC_DIR wwawing-dist wwawing-dist.zip
+
+## acorn-statement バージョンのデプロイをベタ書きしています。
+## 今後、不安定版のプレビューが必要になった時はいい感じに改変してください。
+## HACK: プレビューをデプロイすべきタグ名を、releases.json の releaseUnits[0] から自動で取得して回したい。
+ACORN_STATEMENT_LATEST_VERSION=$(node -e "console.log(require('./releases.json').releaseUnits[0].unstable[0].releases[0].version)")
+wget "https://github.com/WWAWing/WWAWing/releases/download/v$ACORN_STATEMENT_LATEST_VERSION/wwawing-dist.zip"
+unzip -d wwawing-dist wwawing-dist.zip
+
+DEST_DIR="./wwawing.com/acorn-statement"
+cp "$SRC_DIR/mapdata/wwamap.dat" $DEST_DIR
+cp "$SRC_DIR/mapdata/wwa.js" $DEST_DIR
+cp "$SRC_DIR/mapdata/wwa.css" $DEST_DIR
+cp "$SRC_DIR/mapdata/style.css" $DEST_DIR
+cp "$SRC_DIR/mapdata/making.gif" $DEST_DIR
+cp "$SRC_DIR/mapdata/cover.gif" $DEST_DIR
+cp -R "$SRC_DIR/mapdata/audio" $DEST_DIR
+
+rm -rf $SRC_DIR ./wwawing-dist ./wwawing-dist.zip
