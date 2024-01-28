52ad7dd2192fdaae8af3337797692177414e33d2
Aokashi <Aokashi.Towa@Gmail.com>
2024-01-28 22:46:11 +0900

    feat: ピクチャ機能のデモページを作成

--- scripts/deploy-demo.sh
+++ scripts/deploy-demo.sh
@@ -38,11 +38,13 @@ unzip -d wwawing-dist wwawing-dist.zip
 
 DEST_DIR="./wwawing.com/unstable"
 cp "$SRC_DIR/mapdata/wwamap.dat" $DEST_DIR
+cp "$SRC_DIR/mapdata/picture_test.dat" $DEST_DIR
 cp "$SRC_DIR/mapdata/wwa.js" $DEST_DIR
 cp "$SRC_DIR/mapdata/wwa.css" $DEST_DIR
 cp "$SRC_DIR/mapdata/style.css" $DEST_DIR
 cp "$SRC_DIR/mapdata/making.gif" $DEST_DIR
 cp "$SRC_DIR/mapdata/cover.gif" $DEST_DIR
+cp "$SRC_DIR/mapdata/picture_test-picture.json" $DEST_DIR
 cp -R "$SRC_DIR/mapdata/audio" $DEST_DIR
 
 rm -rf $SRC_DIR ./wwawing-dist ./wwawing-dist.zip
